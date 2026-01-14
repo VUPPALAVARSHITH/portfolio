f5b8a21fa70310a5c98e5828f647fdbedeaf0654
VUPPALAVARSHITH <23211a66j6@bvrit.ac.in>
2026-01-14 23:02:31 +0530

    Resume Updated

--- src/App.tsx
+++ src/App.tsx
@@ -179,7 +179,7 @@ function App() {
               View Projects
             </button>
             <a
-              href="https://drive.google.com/file/d/1s8UrRl7Wt60Sr5WEXuyGWOcr-kiezQYP/view?usp=sharing"
+              href="https://drive.google.com/file/d/1f9v6VMRQh7rgL1y48_-JRxyLywuztHnY/view?usp=drive_link"
               target="_blank"
               rel="noopener noreferrer"
               className="px-8 py-3 border border-white/20 text-[#E5E5E5] hover:bg-white/20 rounded-lg font-medium transition-all duration-300 flex items-center justify-center gap-2 backdrop-blur-xl"
